1faf897f53c76d7942a9e60e40e205fc278a58a7
Robert Bubnik <rbubnik@gmail.com>
2023-10-18 19:05:48 -0400

    passing core deliverables

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-function ListingCard() {
+function ListingCard({listing, onDelete}) {
+
+
+ 
+
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <img src={listing.image !== '' ? listing.image : "https://via.placeholder.com/300x300"} alt={"description"} />
       </div>
       <div className="details">
         {true ? (
@@ -13,9 +17,11 @@ function ListingCard() {
         ) : (
           <button className="emoji-button favorite">☆</button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
-        <button className="emoji-button delete">🗑</button>
+        <strong>{listing.description}</strong>
+        <span> · {listing.location}</span>
+        <button
+        onClick={()=> onDelete(listing)}
+        className="emoji-button delete">🗑</button>
       </div>
     </li>
   );
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,37 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, {useEffect, useState} from "react";
+import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
+
+const [listings, setListings] = useState([])
+
+useEffect(()=>{
+  fetch(`http://localhost:6001/listings`)
+  .then(response=> response.json())
+  .then(data=> setListings(data))
+
+  
+},[])
+console.log(listings)
+
+function handleDelete (deletedListing) {
+  
+  fetch(`http://localhost:6001/listings/${deletedListing.id}`,{
+    method:"DELETE",
+    })
+   .then((r) => r.json())
+   .then(() => console.log("deleted!"));
+  
+
+  const updatedListings = listings.filter((listing) => listing !== deletedListing)
+  setListings(updatedListings)
+}
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listings.map((listing)=>{
+        return < ListingCard key={listing.id} listing={listing} onDelete={handleDelete} />})}
       </ul>
     </main>
   );
